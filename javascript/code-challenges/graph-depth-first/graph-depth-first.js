1d8c02e10ada55ab87d2bacc704f8e73d68305fd
Obieda Akilan <obieda.jehad@gmail.com>
2022-06-05 22:25:30 +0300

    graph-depth-first.js

--- javascript/code-challenges/graph-depth-first/graph-depth-first.js
+++ javascript/code-challenges/graph-depth-first/graph-depth-first.js
@@ -89,35 +89,40 @@ class Graph {
         return result;
     }
 }
-let test = new Graph();
-let one = new Vertex("A");
-let two = new Vertex("B");
-let three = new Vertex("C");
-let four = new Vertex("D");
-let five = new Vertex("E");
-let six = new Vertex("F");
-let seven = new Vertex("G");
-let eight = new Vertex("H");
+// let test = new Graph();
+// let one = new Vertex("A");
+// let two = new Vertex("B");
+// let three = new Vertex("C");
+// let four = new Vertex("D");
+// let five = new Vertex("E");
+// let six = new Vertex("F");
+// let seven = new Vertex("G");
+// let eight = new Vertex("H");
 
-test.addVertex(one);
-test.addVertex(two);
-test.addVertex(three);
-test.addVertex(four);
-test.addVertex(five);
-test.addVertex(six);
-test.addVertex(seven);
-test.addVertex(eight);
+// test.addVertex(one);
+// test.addVertex(two);
+// test.addVertex(three);
+// test.addVertex(four);
+// test.addVertex(five);
+// test.addVertex(six);
+// test.addVertex(seven);
+// test.addVertex(eight);
 
-test.addDirectedEdge(one, two);
-test.addDirectedEdge(one, four);
-test.addDirectedEdge(two, four);
-test.addDirectedEdge(two, three);
-test.addDirectedEdge(two, seven);
-test.addDirectedEdge(four, five);
-test.addDirectedEdge(four, six);
-test.addDirectedEdge(four, eight);
-test.addDirectedEdge(six, eight);
+// test.addDirectedEdge(one, two);
+// test.addDirectedEdge(one, four);
+// test.addDirectedEdge(two, four);
+// test.addDirectedEdge(two, three);
+// test.addDirectedEdge(two, seven);
+// test.addDirectedEdge(four, five);
+// test.addDirectedEdge(four, six);
+// test.addDirectedEdge(four, eight);
+// test.addDirectedEdge(six, eight);
 
-console.log(test.depthFirst(one));
+// console.log(test.depthFirst(one));
 
 
+module.exports = {
+    Vertex,
+    Graph,
+
+}
